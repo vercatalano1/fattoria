085e6eeec1fb437ee0d027512f0576bc8433b428
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-03-04 22:57:46 +0100

    Salti e camminata

--- farm/Assets/Scenes/scripts/movement.cs
+++ farm/Assets/Scenes/scripts/movement.cs
@@ -6,17 +6,46 @@ public class movement : MonoBehaviour
 {
    [SerializeField] private float speed;
    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
   private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal")* speed, body.velocity.y);
+        float horizontalinput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalinput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        //flip player when moving left-right
+        if (horizontalinput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalinput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //Set animator parameters 
+        anim.SetBool("run", horizontalinput != 0);
+        anim.SetBool("grounded", grounded);
     }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
 }
