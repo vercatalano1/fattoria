5cd426afbce9037af8dc132354952329413cf0c7
vercatalano1 <ver.catalano1@stud.uniroma3.it>
2024-03-08 19:35:52 +0100

    creazione Login

---
+++ farm/Assets/Scenes/scripts/LoginPage.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using UnityEngine.SceneManagement;
+
+public class LoginPAge : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI Toptext;
+    [SerializeField] TextMeshProUGUI MessageText;
+
+    [Header("Login")]
+    [SerializeField] TMP_InputField EmailLoginInput;
+    [SerializeField] TMP_InputField PasswordLoginInput;
+    [SerializeField] GameObject LoginPage;
+
+    [Header("Register")]
+    [SerializeField] TMP_InputField UsernameRegisterInput;
+    [SerializeField] TMP_InputField EmailRegisterInput;
+    [SerializeField] TMP_InputField PasswordRegisterInput;
+    [SerializeField] GameObject RegisterPage;
+
+    [Header("Recovery")]
+    [SerializeField] TMP_InputField EmailRecoveryInput;
+    [SerializeField] GameObject RecoveryPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Buttom Function
+
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = EmailLoginInput.text,
+            Password = PasswordLoginInput.text,
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+    }
+
+
+    private void OnLoginSuccess(LoginResult Result)
+    {
+        MessageText.text = "Loggin in";
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void RecoverUser()
+    {
+        var request = new SendAccountRecoveryEmailRequest
+        {
+            Email = EmailRecoveryInput.text,
+            TitleId = "E9C70",
+        };
+
+        PlayFabClientAPI.SendAccountRecoveryEmail(request, OnRecoverySuccess, OnErrorRecovery);
+    }
+
+    private void OnErrorRecovery(PlayFabError error)
+    {
+        MessageText.text = "No Email Found";
+    }
+
+    private void OnRecoverySuccess(SendAccountRecoveryEmailResult result)
+    {
+        OpenLoginPage();
+        MessageText.text = "Recovery Mail Sent";
+    }
+
+    public void RegisterUser()
+    {
+        //if password is less
+
+
+        var request = new RegisterPlayFabUserRequest
+        {
+            DisplayName = UsernameRegisterInput.text,
+            Email = EmailRegisterInput.text,
+            Password = PasswordRegisterInput.text,
+
+            RequireBothUsernameAndEmail = false,
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnregisterSuccess, OnError);
+    }
+
+    private void OnError(PlayFabError Error)
+    {
+        MessageText.text = Error.ErrorMessage;
+        Debug.Log(Error.GenerateErrorReport());
+    }
+
+    private void OnregisterSuccess(RegisterPlayFabUserResult Result)
+    {
+        MessageText.text = "New Account Is Created";
+        OpenLoginPage();
+    }
+
+    public void OpenLoginPage()
+    {
+        LoginPage.SetActive(true);
+        RegisterPage.SetActive(false);
+        RecoveryPage.SetActive(false);
+        Toptext.text = "Login";
+    }
+
+    public void OpenRegisterPage()
+    {
+        LoginPage.SetActive(false);
+        RegisterPage.SetActive(true);
+        RecoveryPage.SetActive(false);
+        Toptext.text = "Register";
+    }
+
+    public void OpenRecoveryPage()
+    {
+        LoginPage.SetActive(false);
+        RegisterPage.SetActive(false);
+        RecoveryPage.SetActive(true);
+        Toptext.text = "Recovery";
+
+    }
+
+    #endregion
+}
