bb78969126edb18936e3af1062705fb35e4bf5fe
vercatalano1 <ver.catalano1@stud.uniroma3.it>
2024-03-09 19:53:31 +0100

    miglioramenti

--- farm/Assets/Scenes/scripts/Health/Health.cs
+++ farm/Assets/Scenes/scripts/Health/Health.cs
@@ -49,7 +49,7 @@ public class Health : MonoBehaviour
             if (!dead)
             { 
                 //Player
-                /*if(GetComponent<movement>() != null)
+                if(GetComponent<movement>() != null)
                    GetComponent<movement>().enabled = false;
 
                 //Enemy
@@ -57,10 +57,10 @@ public class Health : MonoBehaviour
                     GetComponentInParent<EnemyPatrol>().enabled = false;
 
                 if(GetComponent<MeleeEnemy>() != null)
-                    GetComponent<MeleeEnemy>().enabled = false;*/
+                    GetComponent<MeleeEnemy>().enabled = false;
 
-                foreach (Behaviour component in components)
-                    component.enabled = false;
+                /*foreach (Behaviour component in components)
+                    component.enabled = false;*/
 
                 anim.SetBool("grounded", true);
                 anim.SetTrigger("die");
