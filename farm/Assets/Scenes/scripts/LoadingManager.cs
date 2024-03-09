72d2e0c74ec4c1e82b96fc24c87dc15d607bb94f
vercatalano1 <ver.catalano1@stud.uniroma3.it>
2024-03-09 17:27:55 +0100

    secondo livello

---
+++ farm/Assets/Scenes/scripts/LoadingManager.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingManager : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        SceneManager.LoadScene(2);
+    }
+}
